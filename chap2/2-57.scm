09f982cdde5a985c7ba42365c499974e36ab6f3b
yamanobori_old <yamanobori_old@yahoo.co.jp>
2017-05-03 09:29:28 +0900

    練習問題 2.57

---
+++ chap2/2-57.scm
@@ -0,0 +1,63 @@
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp) (if (same-variable? exp var) 1 0))
+        ((sum? exp) (make-sum
+                      (deriv (addend exp) var)
+                      (deriv (augent exp) var)))
+        ((product? exp) (make-sum
+                          (make-product
+                            (multiplier exp)
+                            (deriv (multiplicand exp) var))
+                          (make-product
+                            (deriv (multiplier exp) var)
+                            (multiplicand exp))))
+        ((exponentiation? exp) (make-product
+                                 (exponent exp)
+                                 (make-product
+                                   (make-exponentiation (base exp) (make-sum (exponent exp) -1))
+                                   (deriv (base exp) var) ;du/dx
+                                   )))
+        (else
+          (error "unknown expression type : DERIV" exp))))
+(define (variable? e) (symbol? e))
+(define (same-variable? v1 v2) (and (variable? v1) (variable? v2) (eq? v1 v2)))
+(define (sum? e) (and (pair? e) (eq? '+ (car e))))
+(define (addend s) (cadr s))
+(define (augent s) (caddr s))
+(define (product? e) (and (pair? e) (eq? '* (car e))))
+(define (multiplier p) (cadr p))
+(define (multiplicand p) (caddr p))
+(define (=number? exp num) (and (number? exp) (= exp num)))
+(define (exponentiation? e) (and (pair? e) (eq? '** (car e))))
+(define (base e) (cadr e))
+(define (exponent e) (caddr e))
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list '+ a1 a2))))
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+(define (make-exponentiation b e)
+  (cond ((=number? e 0) 1)
+        ((=number? e 1) b)
+        (else (list '** b e))))
+;===============================================================================
+(define (augent s)
+  (if (<= (length s) 3)
+    (caddr s)
+    (cons '+ (cddr s))))
+(define (multiplicand p)
+  (if (<= (length p) 3)
+    (caddr p)
+    (cons '* (cddr p))))
+
+; テスト
+(deriv '(* (* x y) (+ x 3)) 'x)
+(deriv '(* x y (+ x 3)) 'x)
+(deriv '(+ (+ x y) (+ x 3)) 'x)
+(deriv '(+ x y (+ x 3)) 'x)
